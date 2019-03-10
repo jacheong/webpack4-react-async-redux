67c1e865812b189d45e867f3d05192651fa42386
Justin Cheong <js388111@gmail.com>
2019-03-10 18:04:03 +0000

    wepback setup complete

---
+++ webpack.config.common.js
@@ -0,0 +1,36 @@
+module.exports = {
+    entry: {
+        main: './src/index.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(svg|png|jpg|gif)/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]",
+                        outputPath: "img"
+                    }
+                }
+            }
+        ]
+    }
+};
---
+++ webpack.config.dev.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const common = require('./webpack.config.common');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = merge(common, {
+    mode: "development",
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist")   
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true,
+                removeComments: true
+            }
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
+    }
+});
