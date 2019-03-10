1a4641d56a5a855217215c6b910cc27b96a4916f
Justin Cheong <js388111@gmail.com>
2019-03-10 20:10:17 +0000

    react, redux, webpack ready

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import SongList from './SongList';
-import SongDetail from './SongDetail';
 
 const App = () => {
     return (
         <div className="container">
-            <div className="row">
-                <div className="col-md-6">
-                    <SongList />
-                </div>
-                <div className="col-md-6">
-                    <SongDetail />
-                </div>
-            </div>
+            App
         </div>
     ); 
 };
