25b0b3f90bebe6233ecad8a0b5d70c5182609b02
Justin Cheong <js388111@gmail.com>
2019-03-11 00:31:54 +0000

    finished

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPostsAndUsers } from '../actions';
 import './PostList.css';
 import UserHeader from './UserHeader';
 
 class PostList extends Component {
 
     componentDidMount() {
-        this.props.fetchPosts();
+        this.props.fetchPostsAndUsers();
     }
 
     renderList() {
@@ -38,4 +38,4 @@ const mapStateToProps = (state) => {
     };
 };
 
-export default connect( mapStateToProps, {fetchPosts: fetchPosts} )(PostList);    
+export default connect( mapStateToProps, {fetchPostsAndUsers: fetchPostsAndUsers} )(PostList);    
